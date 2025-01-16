1806fe601d47824e045a68ff64e897fa59c1e6b1
Fabienholert <fabienholert@gmail.com>
2025-01-16 18:52:52 +0100

    mise a jour bug avec Apropos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import {Router, Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home';
-import APropos from './pages/APropos';
-import Erreur404 from './pages/Erreur404';
-import FicheLogement from './pages/FicheLogement';
+import Home from '../src/components/pages/Home/index';
+import Apropos from '../src/components/pages/Apropos/index';
+import Erreur404 from '../src/components/pages/Erreur404/index';
+import FicheLogement from '../src/components/pages/FicheLogement/index';
 
-function App(){
+export default function App(){
   return (
   <Router>
     <Routes>
       <Route path="pages/..." element={<Home />} />
-      <Route path="pages/..." element={<APropos />} />
+      <Route path="pages/..." element={<Apropos />} />
       <Route path="/pages/.../" element={<Erreur404 />} />
       <Route path="pages/..." element={<FicheLogement />} />
-    </Routes>,
+    </Routes>
     </Router>
   )
 }
-export default App
