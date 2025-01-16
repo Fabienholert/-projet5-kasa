031bc5a6752cef7a630c5a123ed65a164eee9496
Fabienholert <fabienholert@gmail.com>
2025-01-16 16:51:21 +0100

    mise a jour projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
- function App() {
+import {Router, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home';
+import APropos from './pages/APropos';
+import Erreur404 from './pages/Erreur404';
+import FicheLogement from './pages/FicheLogement';
 
+function App(){
   return (
-    <>
-     
-    </>
+  <Router>
+    <Routes>
+      <Route path="pages/..." element={<Home />} />
+      <Route path="pages/..." element={<APropos />} />
+      <Route path="/pages/.../" element={<Erreur404 />} />
+      <Route path="pages/..." element={<FicheLogement />} />
+    </Routes>,
+    </Router>
   )
 }
 export default App
