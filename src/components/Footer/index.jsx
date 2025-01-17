98e76e3038d0e394d58bfeae7d5cfc8d017fd39a
Fabienholert <fabienholert@gmail.com>
2025-01-17 15:25:33 +0100

    mise a jour projet ajout des logos png

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import Logoblanc from '../../assets/Logoblanc'
+
+export default function Footer () {
+    return (
+        <div className='Footer'>
+           <p>K{Logoblanc}Sa</p>
+           <p>© 2020 Kasa. All rights reserved</p>
+
+        </div>
+    )
+}
