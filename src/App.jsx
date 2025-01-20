6412cf9a6a47e4dbc14ad60e0e6f6afe33280eab
Fabienholert <fabienholert@gmail.com>
2025-01-20 22:06:41 +0100

    mise a jour session de mentorat

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../src/components/pages/Home/index';
 import Apropos from '../src/components/pages/Apropos/index';
 import Erreur404 from '../src/components/pages/Erreur404/index';
 import Fichelogement from '../src/components/pages/Fichelogement/index';
+import Header from './components/Header/index'
+import Footer from './components/Footer/index'
+
 
 export default function App(){
   return (
     <div className='App'>
-  <Router>
+  <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Apropos" element={<Apropos />} />
-      <Route path="*" element={<Erreur404 />} />
       <Route path="/Fichelogement" element={<Fichelogement />} />
+      <Route path="*" element={<Erreur404 />} />
     </Routes>
-    </Router>
+    <Footer/>
+    </BrowserRouter>
     </div>
   );
 }
