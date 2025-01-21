a993515d77574955bb059eaab7f952644eb2877f
Fabienholert <fabienholert@gmail.com>
2025-01-21 23:26:31 +0100

    mise a jour de A propos.css et mise en place de Home.css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
+import imageHome from '../../assets/imageHome.jpg'
+import './Home.css'
+
 export default function Home () {
     return (
         <>
-
-        <div className=''>
+        <div>
+        <img src= {imageHome} alt = 'image en fond d écran représentant des rochers' className='imageHome' />
+        <div className='textehome'>
             Chez vous, partout et ailleurs
+        </div>
         </div>
         <div>Card</div>
 
