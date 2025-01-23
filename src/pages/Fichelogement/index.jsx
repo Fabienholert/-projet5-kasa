ea62271cf15684ee4e31f9c56c068506148326b7
Fabienholert <fabienholert@gmail.com>
2025-01-23 17:26:09 +0100

    mise en place de la fiche de logement

--- src/pages/Fichelogement/index.jsx
+++ src/pages/Fichelogement/index.jsx
@@ -1,13 +1,14 @@
+import data from '../../assets/logement.json'
 
 
 
-export default function Fichelogement() {
+export default function ficheLogement(props) {
     return (
-        <>
-
-        <div className=''>
-            
-        </div>,
-        </>
-    )
-}
+        <div>
+            <img src = {props.pictures} alt = "" className='carroussel'/>
+        </div>)};        
+           <div className="container-card">
+          {data.map((ficheLogement, index) => (
+              <ficheLogement key={index}  title={ficheLogement.title} location={ficheLogement.location} host={ficheLogement.host} rating={ficheLogement.rating} tag={ficheLogement.tag} description={ficheLogement.description} equipment={ficheLogement.equipment}  />
+          ))}
+      </div>
