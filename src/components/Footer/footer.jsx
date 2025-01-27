6c9b253ab3b4fdc4dc6421ee088d33b99b57a2ca
Fabienholert <fabienholert@gmail.com>
2025-01-27 22:05:50 +0100

    mise a jour mentorat

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,9 +3,9 @@ import './footer.css'
 
 export default function Footer () {
     return (
-        <div className='Footer'>
+        <footer className='Footer'>
            <img src ={Logob} alt ='logo Kasa en blanc' className='logo-blanc'/>
            <p>© 2020 Kasa. All rights reserved</p>
-        </div>
+        </footer>
     )
 }
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
 import { Outlet } from "react-router-dom";
 
 
@@ -10,9 +10,7 @@ export default function Layout (){
       <main>
         <Outlet /> {/* Les routes enfants seront affichées ici */}
       </main>
-      <footer>
       <Footer/>
-      </footer>
     </>
   );
 };
