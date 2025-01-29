a32ebd5a5f3d2e87a1c1bc76de6e6e0e5e0e1734
Fabienholert <fabienholert@gmail.com>
2025-01-29 17:52:17 +0100

    mise a jour de la collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,8 +1,22 @@
-export default function Collapse() {
+import { useState } from 'react';
+import arrowdown from '../../assets/arrowdown.png';
+import arrowup from '../../assets/arrowup.png';
+
+
+export default function Collapse({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-      <button className='collapsetitle'>
-      <div className='collapsecontent'>
-        </div>
+    <div className="collapse-container">
+      <button className='collapsetitle' onClick={toggleCollapse}>
+        {title}
+        <img src={isOpen ? arrowdown : arrowup} alt="arrow" className='arrow' />
       </button>
+      {isOpen && <div className="collapsecontent">{children}</div>}
+    </div>
   );
 }
--- src/pages/Apropos/apropos.jsx
+++ src/pages/Apropos/apropos.jsx
@@ -2,24 +2,23 @@ import imageApropos from '../../assets/imageapropos.jpg'
 import Section from '../../components/Section/section'
 import './Apropos.css'
 import Collapse from '../../components/Collapse/Collapse'
-import apropos from '../../assets/apropos.json'
-
+import detailsapropos from '../../assets/apropos.json'
 
 export default function Apropos() {
     return (
         <>
-        
             <div>
                 <Section imgSrc={imageApropos} alt="Image de la page d'accueil" />
             </div>
             <div className='collapse-container'>
-             {apropos.map((apropos) => (
+             {detailsapropos.map((apropos) => (
                               <Collapse
                                   key={apropos.title}
-                                  title={apropos.title}
-                                  description={apropos.description}
-                              />
-                                  
-            ))}
+                                  title={apropos.title}>
+                                 <p>{apropos.description}</p>
+                                  </Collapse>
+             ))}
         </div>
-        </>)}
+        </>
+    )
+}
