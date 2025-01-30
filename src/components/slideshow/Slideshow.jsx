265eb45de93900d090831bde3e0760f63eee3095
Fabienholert <fabienholert@gmail.com>
2025-01-30 16:36:06 +0100

    mise a jour de slideshow

---
+++ src/components/slideshow/Slideshow.jsx
@@ -0,0 +1,11 @@
+import arrowprev from '../../src/assets/arrowprev'
+import arrownext from '../../src/assets/arrownext'
+
+export default function caroussel (){
+    return (
+    <div>
+        <img src ={arrowprev} alt = '' className='' />
+        
+        <img src ={arrownext} alt = '' className='' />
+    </div>
+)};
--- src/pages/Fichelogement/fichelogement.jsx
+++ src/pages/Fichelogement/fichelogement.jsx
@@ -3,18 +3,11 @@ import Collapse from '../../components/Collapse/Collapse';
 
 const informations= data
 
-export default function FicheLogement({pictures, title, location, host, rating, tag, description, equipments }) {
+export default function Fichelogement(data) {
   return (
     <div key={informations.id}>
-    
         
             <img src={pictures} alt={title} className='carroussel'/>
-        
-      <h2>{informations.title}</h2>
-      <p>Location: {informations.location}</p>
-      <p>Host: {informations.host.name}</p>
-      <p>Rating: {informations.rating}</p>
-        <p>tag: {informations.tag}</p>
       <Collapse>description {informations.description}</Collapse>
       <Collapse>equipements {informations.equipments}</Collapse>
     </div>
