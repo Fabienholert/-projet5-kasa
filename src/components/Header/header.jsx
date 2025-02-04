d9cd1b6779ba2c8e362376923236ebb1e9d08c9c
Fabienholert <fabienholert@gmail.com>
2025-02-04 16:33:10 +0100

    mise a jour header pour scss

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -7,9 +7,9 @@ export default function Header () {
     return (
         <header className='header'>
             <img src= {Logor} alt = 'logo Kasa en rouge' className='header__logo' />
-            <nav className='header__link'>
-                    <NavLink to="/"> Accueil </NavLink>
-                    <NavLink to="/Apropos"> A Propos </NavLink>      
+            <nav className='header__nav'>
+                    <NavLink to="/" className='header__link'> Accueil </NavLink>
+                    <NavLink to="/Apropos" className='header__link'> A Propos </NavLink>      
             </nav>
 
         </header>
