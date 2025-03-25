ff18c0f4571810cd9a4b5f8ac257925cb7d15596
Fabienholert <fabienholert@gmail.com>
2025-03-26 00:08:26 +0100

    ajout de la configuration de déploiement et mise à jour des chemins de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './layout'
-import Home from './pages/Home/home';
-import Apropos from './pages/Apropos/apropos';
-import Erreur404 from './pages/Erreur404/erreur404';
-import Fichelogement from './pages/Fichelogement/fichelogement';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layout";
+import Apropos from "./pages/Apropos/apropos";
+import Erreur404 from "./pages/Erreur404/erreur404";
+import Fichelogement from "./pages/Fichelogement/fichelogement";
+import Home from "./pages/Home/home";
 
-export default function App(){
+export default function App() {
   return (
-    <div className='App'>
-  <BrowserRouter>
-    <Routes>
-      <Route element= {<Layout/>}>
-        <Route path ="/" element={<Home />} />
-        <Route path="/Apropos" element={<Apropos />} />
-        <Route path="/Fichelogement/:id" element={<Fichelogement />}/>
-        <Route path="*" element={<Erreur404 />} />
-      </Route>    
-    </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter basename="/kasa/">
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/Apropos" element={<Apropos />} />
+            <Route path="/Fichelogement/:id" element={<Fichelogement />} />
+            <Route path="*" element={<Erreur404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
